9f4d1035fc03a5e75e3b244defc87ae7ad026327
Maria Zinkovskaya <my.project.git@gmail.com>
2020-05-29 15:20:52 +0300

    Added 9, 10 tasks

--- js/task-10.js
+++ js/task-10.js
@@ -3,14 +3,21 @@
 import users from './users.js';
 
 const getSortedUniqueSkills = users => {
-  const sortedSkills = users.reduce((allSkills, user) => { 
-  allSkills.push(...user.skills);
+  const sortedSkills = users
+    .reduce((allSkills, user) => {
+      allSkills.push(...user.skills);
 
-  return allSkills;
+      return allSkills;
+    }, [])
 
-}, []);
+    .filter((skill, index, skills) => {
+      return index === skills.indexOf(skill);
+    })
 
+    .sort();
 
+  return sortedSkills;
+};
 
-/*console.log(getSortedUniqueSkills(users));
-// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]*/
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
--- js/task-9.js
+++ js/task-9.js
@@ -11,5 +11,5 @@ const getNamesSortedByFriendsCount = users => {
   return sortedFriends;
 };
 
-console.table(getNamesSortedByFriendsCount(users));
+console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
